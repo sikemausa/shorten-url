e30570c1ea11156f3d2627f19d6f88aae68940cc
Mike Sausa <mikejsausa@gmail.com>
2016-11-29 14:56:29 -0700

    set server up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "2",
+      url: 'http://www.dirtymike.com',
+    }
+  }
+
+post(){
+  axios.post(`/urls`, {
+      id: this.state.id,
+      url: this.state.url
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form>
+          <label>
+            <input type="text" />
+          </label>
+          <input type="button" onClick={ () => this.post()} />
+        </form>
       </div>
     );
   }
---
+++ src/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const md5 = require('md5');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 3001);
+app.locals.title = 'Secret Box';
+app.locals.urls = [];
+
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
+
+app.get('/urls/:id', (request, response) => {
+  const { id } = request.params;
+  const message = app.locals.urls[id];
+
+  if (!message) { return response.sendStatus(404); }
+
+  response.json({ id, message });
+});
+
+app.get('/urls', (request, response) => {
+    response.send(app.locals.urls);
+});
+
+  app.post('/urls', (request, response) => {
+  const data = request.body;
+  data.id = data.id || Date.now();
+  app.locals.urls.push(data);
+  response.status(201).send({ data });
+});
+
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
