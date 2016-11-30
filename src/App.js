2c35fe4afc62a4613ca9a7baa68b603bc9f77f85
Mike Sausa <mikejsausa@gmail.com>
2016-11-29 17:06:43 -0700

    rendered urls

--- src/App.js
+++ src/App.js
@@ -8,16 +8,35 @@ class App extends Component {
     this.state = {
       id: "",
       url: "",
+      shortenedUrl: "",
+      clicks: "",
+      storedUrls: ""
     };
   }
+
+componentDidMount() {
+  this.get();
+}
 
 post(){
   axios.post(`/urls`, {
       id: this.state.id,
-      url: this.state.url
+      url: this.state.url,
+      shortenedUrl: this.state.shortenedUrl,
+      clicks: this.state.clicks
   })
   .then(function (response) {
     console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+get(){
+  axios.get(`/urls`).then((response) => {
+      console.log(response);
+      this.setState({ storedUrls: response.data });
   })
   .catch(function (error) {
     console.log(error);
@@ -29,18 +48,38 @@ updateUrl(e) {
   this.setState({ url: urlInput });
 }
 
+updateShortenedUrl(e) {
+  const shortenedUrlInput = e.target.value;
+  this.setState({ shortenedUrl: shortenedUrlInput });
+}
+
   render() {
-    const { id, url } = this.state;
+    const { id, url, shortenedUrl, storedUrls } = this.state;
     return (
       <div className="App">
         <form>
           <label>
             <input type="text"
                    value={url}
-                   onChange={ (e) => this.updateUrl(e) }/>
+                   onChange={ e => this.updateUrl(e) }/>
+            <input type="text"
+                   value={shortenedUrl}
+                   onChange= { e => this.updateShortenedUrl(e) } />
           </label>
           <input type="button" onClick={ () => this.post()} />
         </form>
+        <div>
+          { storedUrls ?
+            storedUrls.map((website) => {
+              return (
+                <div key={website.id}>
+                <h1>Url: {website.url}</h1>
+                <h1> Shortened Url: { website.shortenedUrl }</h1>
+                </div>
+              )
+            })
+            :null }
+        </div>
       </div>
     );
   }
