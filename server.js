18d20f581298485526063f965e8ef7a1cfa690c2
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-11-30 15:34:31 -0700

    test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const md5 = require('md5');
 const crc = require('crc');
 
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
