6b0a13f9dae34a7797d1799e947f1f2eb6a4cdd3
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-12-01 12:42:43 -0700

    added more testing

--- src/App.test.js
+++ src/App.test.js
@@ -18,3 +18,52 @@ describe('GET /', () => {
        .expect(200, done);
    });
  });
+
+ describe('GET all urls', () => {
+
+  beforeEach(() => {
+    app.locals.urls = [{ id: 1,
+                         url: 'http://www.google.com',
+                         shortenedUrls: 'http://localhost:8080/acf785',
+                         clicks: 3
+                       }];
+  });
+
+  afterEach(() => {
+    app.locals.urls = [];
+  });
+
+  it('should return a 201 status code', (done) => {
+    request(app)
+      .get('/urls')
+      .expect(201, done);
+  });
+
+  it('should return a url stored in app.locals.urls', (done) => {
+    request(app)
+      .get('/urls')
+      .expect(201, {
+        urls: app.locals.urls
+      }, done);
+  });
+});
+
+describe('POST a url', () => {
+
+  beforeEach(() => {
+    app.locals.urls = [];
+  });
+
+  it('should create a new url', (done) => {
+    const url = 'www.google.com';
+
+    request(app)
+      .post('/urls')
+      .send({ url })
+      .expect(201)
+      .end(() => {
+        assert.equal(app.locals.urls.length, 1);
+        done();
+      });
+  });
+});
